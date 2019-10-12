5da0999ae7e8b013ea7f9038f52f85617bb3bbf1
vkopendoh <vkopendoh@bk.ru>
2019-10-12 23:45:47 +0300

    2 5 dependency injection

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -21,6 +21,10 @@ public class UserService {
         this.repository = repository;
     }
 
+    public void setRepository(UserRepository repository) {
+        this.repository = repository;
+    }
+
     public User create(User user) {
         return repository.save(user);
     }
