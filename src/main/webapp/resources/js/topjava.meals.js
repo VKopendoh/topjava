c26803a124c0cf91472edde286242b23fa5b9d56
vkopendoh <vkopendoh@bk.ru>
2019-12-13 18:05:52 +0300

    10 04 jquery converters

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -1,25 +1,25 @@
-var mealAjaxUrl = "ajax/profile/meals/";
-
 function updateFilteredTable() {
     $.ajax({
         type: "GET",
-        url: mealAjaxUrl + "filter",
+        url: "ajax/profile/meals/filter",
         data: $("#filter").serialize()
     }).done(updateTableByData);
 }
 
 function clearFilter() {
     $("#filter")[0].reset();
-    $.get(mealAjaxUrl, updateTableByData);
+    $.get("ajax/profile/meals/", updateTableByData);
 }
 
 $(function () {
     makeEditable({
-        ajaxUrl: mealAjaxUrl,
-        datatableOpts: {
+        ajaxUrl: "ajax/profile/meals/",
+        datatableApi: $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
             "columns": [
                 {
-                    "data": "dateTimeUI"
+                    "data": "dateTime"
                 },
                 {
                     "data": "description"
@@ -28,13 +28,11 @@ $(function () {
                     "data": "calories"
                 },
                 {
-                    "render": renderEditBtn,
-                    "defaultContent": "",
+                    "defaultContent": "Edit",
                     "orderable": false
                 },
                 {
-                    "render": renderDeleteBtn,
-                    "defaultContent": "",
+                    "defaultContent": "Delete",
                     "orderable": false
                 }
             ],
@@ -43,60 +41,8 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ],
-            "createdRow": function (row, data, dataIndex) {
-                $(row).attr("data-mealExcess", data.excess);
-            },
-        },
+            ]
+        }),
         updateTable: updateFilteredTable
-    });
-
-//  http://xdsoft.net/jqplugins/datetimepicker/
-    var startDate = $('#startDate');
-    var endDate = $('#endDate');
-    startDate.datetimepicker({
-        timepicker: false,
-        format: 'Y-m-d',
-        formatDate: 'Y-m-d',
-        onShow: function (ct) {
-            this.setOptions({
-                maxDate: endDate.val() ? endDate.val() : false
-            })
-        }
-    });
-    endDate.datetimepicker({
-        timepicker: false,
-        format: 'Y-m-d',
-        formatDate: 'Y-m-d',
-        onShow: function (ct) {
-            this.setOptions({
-                minDate: startDate.val() ? startDate.val() : false
-            })
-        }
-    });
-
-    var startTime = $('#startTime');
-    var endTime = $('#endTime');
-    startTime.datetimepicker({
-        datepicker: false,
-        format: 'H:i',
-        onShow: function (ct) {
-            this.setOptions({
-                maxTime: endTime.val() ? endTime.val() : false
-            })
-        }
-    });
-    endTime.datetimepicker({
-        datepicker: false,
-        format: 'H:i',
-        onShow: function (ct) {
-            this.setOptions({
-                minTime: startTime.val() ? startTime.val() : false
-            })
-        }
-    });
-
-    $('#dateTime').datetimepicker({
-        format: 'Y-m-d H:i'
     });
 });
